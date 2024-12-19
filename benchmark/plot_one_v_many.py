f27fafe63e2f8a2f75f36bf670aa32f10fdc47ae
Mahmudur <mahmudhera93@gmail.com>
2024-12-18 19:26:18 -0500

    added benchmarking results for one v many, updated plot scripts

--- benchmark/plot_one_v_many.py
+++ benchmark/plot_one_v_many.py
@@ -7,12 +7,14 @@ plt.rcParams['font.family'] = 'Arial'
 # Load the data
 df_compare = pd.read_csv('benchmark_results_compare_one_v_many.csv')
 df_multisearch = pd.read_csv('benchmark_results_multisearch_one_v_many.csv')
+df_prefetch_lm = pd.read_csv('benchmark_results_this_prefetch.csv')
 
 # col names: ref_filesize,cpu_time,wall_clock_time,peak_memory_usage
 
 # plot wall-clock time against reference file size
 plt.plot(df_compare['ref_filesize'], df_compare['wall_clock_time'], label='compare (cpp)', marker='o')
 plt.plot(df_multisearch['ref_filesize'], df_multisearch['wall_clock_time'], label='multisearch (sourmash)', marker='o')
+plt.plot(df_prefetch_lm['ref_filesize'], df_prefetch_lm['wall_clock_time'], label='prefetch-lm (cpp)', marker='o')
 
 plt.xlabel('Reference list size')
 plt.ylabel('Wall-clock time (s)')
@@ -26,6 +28,7 @@ plt.savefig('plots/benchmark_results_compare_one_v_many_wall_clock_time.pdf')
 plt.clf()
 plt.plot(df_compare['ref_filesize'], df_compare['peak_memory_usage'], label='compare (cpp)', marker='o')
 plt.plot(df_multisearch['ref_filesize'], df_multisearch['peak_memory_usage'], label='multisearch (sourmash)', marker='o')
+plt.plot(df_prefetch_lm['ref_filesize'], df_prefetch_lm['peak_memory_usage'], label='prefetch-lm (cpp)', marker='o')
 
 plt.xlabel('Reference list size')
 plt.ylabel('Peak memory usage (GB)')
